3e820f936dd3b077badd5d16481f05d0422d3bd4
Luciano Portales <lportales655@gmail.com>
2021-07-26 03:14:16 -0400

    Formulario con html, css y ts

---
+++ js/app.js
@@ -0,0 +1,39 @@
+"use strict";
+var formulario = document.getElementById("formulario");
+var nombre = formulario.elements[0];
+var apellidos = formulario.elements[1];
+var rut = formulario.elements[2];
+var email = formulario.elements[3];
+var telefono = formulario.elements[4];
+formulario.addEventListener("submit", function (event) {
+    var errorT = document.getElementById('errorRut');
+    errorT.style.display = 'none';
+    var errorR = document.getElementById('errorTelefono');
+    errorR.style.display = 'none';
+    if (validarNumero(telefono.value) && validarRut(rut.value)) {
+        var mensaje = document.getElementById("mensaje-exito");
+        formulario.style.display = "none";
+        mensaje.style.display = "flex";
+        var msg = document.createElement("p");
+        msg.innerHTML = "Hemos  recibido  sus  datos,  pronto  nos estaremos comunicando con usted.";
+        mensaje.appendChild(msg);
+        msg.className = "exito";
+    }
+    event.preventDefault();
+});
+var validarNumero = function (telefono) {
+    if (telefono.length != 9) {
+        var error = document.getElementById('errorTelefono');
+        error.style.display = 'block';
+        return false;
+    }
+    return true;
+};
+var validarRut = function (rut) {
+    if (rut.slice(0, -2).length != 7) {
+        var error = document.getElementById('errorRut');
+        error.style.display = 'block';
+        return false;
+    }
+    return true;
+};
---
+++ ts/app.ts
@@ -0,0 +1,43 @@
+let formulario:any = document.getElementById("formulario");
+const nombre = formulario.elements[0]
+const apellidos = formulario.elements[1]
+const rut = formulario.elements[2]
+const email = formulario.elements[3]
+const telefono = formulario.elements[4]
+
+formulario.addEventListener("submit", (event:any) => {
+    let errorT:any = document.getElementById('errorRut');
+    errorT.style.display = 'none';
+    let errorR:any = document.getElementById('errorTelefono');
+    errorR.style.display = 'none';
+
+    if (validarNumero(telefono.value) && validarRut(rut.value)) {
+        let mensaje:any = document.getElementById("mensaje-exito")
+        formulario.style.display = "none";
+        mensaje.style.display = "flex";
+
+        let msg = document.createElement("p")
+        msg.innerHTML = "Hemos  recibido  sus  datos,  pronto  nos estaremos comunicando con usted."
+        mensaje.appendChild(msg)
+        msg.className = "exito"
+    }
+    event.preventDefault();
+});
+
+const validarNumero = (telefono:string):boolean => {
+    if (telefono.length != 9) {
+        let error:any = document.getElementById('errorTelefono');
+        error.style.display = 'block';
+        return false;
+    }
+    return true;
+} 
+
+const validarRut = (rut:string):boolean => {
+    if (rut.slice(0,-2).length != 7) {
+        let error:any = document.getElementById('errorRut');
+        error.style.display = 'block';
+        return false;
+    }
+    return true;
+}
